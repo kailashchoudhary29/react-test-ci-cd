56b969f3fad9bbe250c3afa19b888d370e9d6dc7
kailash <user.email>
2022-03-30 19:53:03 +0530

    adding funct comp

---
+++ src/component/Functional_Comp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Function_Comp() {
+    return (
+        <div>
+            <h1>
+                <span>function Component</span>
+            </h1>
+        </div>
+    )
+}
+
+export default Function_Comp;
